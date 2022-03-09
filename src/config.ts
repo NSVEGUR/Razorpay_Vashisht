7c3db7bffb01fbe493afc6d8eef15bd589a8cbaf
NSVEGUR <nsvegur@gmail.com>
2022-03-10 01:17:08 +0530

    resolving heroku config port error

---
+++ src/config.ts
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "./../config.env") });
+
+interface ENV {
+  NODE_ENV: string | undefined;
+  PORT: number | undefined;
+}
+
+interface Config {
+  NODE_ENV: string;
+  PORT: number;
+}
+
+// Loading process.env as ENV interface
+
+const getConfig = (): ENV => {
+  return {
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT ? Number(process.env.PORT) : undefined,
+  };
+};
+
+const getSanitzedConfig = (config: ENV): Config => {
+  for (const [key, value] of Object.entries(config)) {
+    if (value === undefined) {
+      throw new Error(`Missing key ${key} in config.env`);
+    }
+  }
+  return config as Config;
+};
+
+const config = getConfig();
+
+const sanitizedConfig = getSanitzedConfig(config);
+
+export default sanitizedConfig;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app";
+import config from "./config";
 
-const port = 3000;
+const port = config.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening from port ${port}`);
